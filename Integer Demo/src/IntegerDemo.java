d956bfc41532ef8e29b565158953498e998f0022
PontiacBrad <bradley.gray@stu.laroche.edu>
2018-03-10 18:56:09 -0500

    Initial commit of Integer Demo

---
+++ Integer Demo/src/IntegerDemo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @author Bradley
+ *This class tests the max and min values of all Numeric types
+ */
+public class IntegerDemo 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Integer max is " + Integer.MAX_VALUE);
+		System.out.println("Integer min is " + Integer.MIN_VALUE);
+		System.out.println("Byte max is " + Byte.MAX_VALUE);
+		System.out.println("Byte min is " + Byte.MIN_VALUE);
+		System.out.println("Short max is " + Short.MAX_VALUE);
+		System.out.println("Short min is " + Short.MIN_VALUE);
+		System.out.println("Long max is " + Long.MAX_VALUE);
+		System.out.println("Long min is " + Long.MIN_VALUE);
+	}
+}

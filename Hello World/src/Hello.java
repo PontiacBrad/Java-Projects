6594b524ea0a6c40bb26368ecbcd456fcfc9f69c
PontiacBrad <bradley.gray@stu.laroche.edu>
2018-03-13 12:03:19 -0400

    Initial commit of Hello World

---
+++ Hello World/src/Hello.java
@@ -0,0 +1,18 @@
+/**
+ * The program shall display:
+     a. my name
+     b. my major
+     c. my hobby
+ * 
+ * @author Lab Team (implementing textbook program: Horsteann)
+ * @version 1-14-15
+ */
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Bradley Gray");
+        System.out.println("Computer Science");
+        System.out.println("Playing videogames");
+    }
+}

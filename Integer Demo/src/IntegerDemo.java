ff68b9d8259fce40ede713b3d8f18bdd5a8263c9
PontiacBrad <bradley.gray@stu.laroche.edu>
2018-06-18 19:25:32 -0400

    Converted program output to output to a file

--- Integer Demo/src/IntegerDemo.java
+++ Integer Demo/src/IntegerDemo.java
@@ -1,19 +1,25 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 /**
- * 
  * @author Bradley
  *This class tests the max and min values of all Numeric types
  */
 public class IntegerDemo 
 {
-	public static void main(String[] args)
+	public static PrintWriter output;
+
+	public static void main(String[] args) throws FileNotFoundException
 	{
-		System.out.println("Integer max is " + Integer.MAX_VALUE);
-		System.out.println("Integer min is " + Integer.MIN_VALUE);
-		System.out.println("Byte max is " + Byte.MAX_VALUE);
-		System.out.println("Byte min is " + Byte.MIN_VALUE);
-		System.out.println("Short max is " + Short.MAX_VALUE);
-		System.out.println("Short min is " + Short.MIN_VALUE);
-		System.out.println("Long max is " + Long.MAX_VALUE);
-		System.out.println("Long min is " + Long.MIN_VALUE);
+		output = new PrintWriter("Output.txt");
+		output.println("Integer max is " + Integer.MAX_VALUE);
+		output.println("Integer min is " + Integer.MIN_VALUE);
+		output.println("Byte max is " + Byte.MAX_VALUE);
+		output.println("Byte min is " + Byte.MIN_VALUE);
+		output.println("Short max is " + Short.MAX_VALUE);
+		output.println("Short min is " + Short.MIN_VALUE);
+		output.println("Long max is " + Long.MAX_VALUE);
+		output.println("Long min is " + Long.MIN_VALUE);
+		output.close();
+		System.out.println("See file Output.txt for the output from this program");
 	}
 }

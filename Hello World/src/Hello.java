a1e84d1ceb17043d86f69f76e3180a5665870ad5
PontiacBrad <bradley.gray@stu.laroche.edu>
2018-06-20 21:44:43 -0400

    Modified program to prompt for user input and spit the input back out to the user

--- Hello World/src/Hello.java
+++ Hello World/src/Hello.java
@@ -1,18 +1,27 @@
+import java.util.Scanner;
+
 /**
  * The program shall display:
      a. my name
      b. my major
      c. my hobby
  * 
- * @author Lab Team (implementing textbook program: Horsteann)
+ * @author Lab Team (implementing textbook program: Horstmann)
  * @version 1-14-15
  */
 public class Hello
 {
     public static void main(String[] args)
     {
-        System.out.println("Bradley Gray");
-        System.out.println("Computer Science");
-        System.out.println("Playing videogames");
+    	Scanner input = new Scanner(System.in);
+    	System.out.print("Enter your name: ");
+    	String name = input.nextLine();
+    	System.out.print("Enter your major: ");
+    	String major = input.nextLine();
+    	System.out.println("Enter your favorite hobby: ");
+    	String hobby = input.nextLine();
+        System.out.println(name);
+        System.out.println(major);
+        System.out.println(hobby);
     }
 }
